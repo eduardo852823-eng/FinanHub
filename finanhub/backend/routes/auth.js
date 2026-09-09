bbfd9982d6e63c77602e66f563bf773efdf3381a
Eduardo <eduardo852823@gmail.com>
2026-09-09 15:55:35 -0400

    Delete finanhub directory

--- finanhub/backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/cadastro', authController.cadastrar);
-router.post('/login', authController.login);
-router.post('/google', authController.loginGoogle);
-
-module.exports = router;
